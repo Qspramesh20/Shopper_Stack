af06848a0dad84709a28ad853d5e00ee3bd5ee7b
user <user@DESKTOP-CDL8RMF>
2024-05-08 09:04:43 +0530

    First commit

---
+++ Fireflink/src/Sample/num2.java
@@ -0,0 +1,14 @@
+package Sample;
+
+public class num2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int a=5;a>=1;a--)
+		{
+			System.out.println(a);
+		}
+
+	}
+
+}
